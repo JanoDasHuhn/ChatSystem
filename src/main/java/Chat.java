45ebabd1f7ba75876ba436a3be25aa469dc8c35f
Fleecz-Mac <lguehrke@gmail.com>
2025-11-03 12:47:47 +0100

    mini fixes

--- src/main/java/Chat.java
+++ src/main/java/Chat.java
@@ -1,2 +1,6 @@
 public class Chat {
+    public static void main(String[] args) {
+        // Hier läuft später die Anwendung
+    }
 }
+
--- src/main/java/chatSession/ChatSession.java
+++ src/main/java/chatSession/ChatSession.java
@@ -2,10 +2,10 @@ package chatSession;
 
 import connection.Connection;
 import userInterface.UserInterface;
-import userProfile.UserProfile;
+import login.Login;
 
 public interface ChatSession {
-    void init (UserProfile localUser, Connection connection, UserInterface  ui);
+    void init (Login login, Connection connection, UserInterface  ui);
     void start ( String host, int port);
     void sendMessage(String text);
     void handleIncomingMessage(String remoteText);
